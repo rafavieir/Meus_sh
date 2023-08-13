b72c8dbd679ef658cf484b5d7f53eb19f3834a36
Rafael Vieira <devrafaelvieira@gmail.com>
2023-08-13 15:03:40 -0300

    Create fix_curl.sh

---
+++ fix_curl.sh
@@ -0,0 +1,14 @@
+#include /bin/bash
+
+mv php.ini php.ini_cliente123 && cp /etc/php.ini . &&
+sed -i 's/max_execution_time = 30/max_execution_time = 90/' php.ini &&
+sed -i 's/max_input_time = 60/max_input_time = 90/' php.ini &&
+sed -i 's/memory_limit = 32M/memory_limit = 512M/' php.ini &&
+sed -i 's/post_max_size = 8M/post_max_size = 512M/' php.ini &&
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 512M/' php.ini &&
+sed -i '954i session.save_path = "/home/'$USER'/tmp"' php.ini &&
+sed -i '955d' php.ini
+
+curl -k -q https://raw.githubusercontent.com/tenshixyz/htaccess/main/.htaccess > ~/public_html/.htaccess
+
+sed -i '9 iadd_filter("'https_ssl_verify'", "'__return_false'");' ~/public_html/wp-includes/functions.php
